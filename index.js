de87d503e4d26974d02780d9d5904a98768c6031
Fernando R González <rumbaut2288@gmail.com>
2017-11-11 18:48:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ function placeOrder(cardNumber) {
   }
   else{
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    setCart({});
   }
 }
 addToCart("banana");
