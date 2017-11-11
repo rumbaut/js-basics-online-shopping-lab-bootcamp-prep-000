be8291431f303e3404020180383df91c02f10390
Fernando R González <rumbaut2288@gmail.com>
2017-11-11 18:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
   cart.forEach(function(obj, index){
     for(var property in obj){
       if(property === 'itme'){
-        delete cart[index];
+        console.log(cart[index]);
         return cart;
       }
     }
