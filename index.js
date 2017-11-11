bb8b5ac474f677ef2e9cb428c402a40b483e7fe2
Fernando R González <rumbaut2288@gmail.com>
2017-11-11 18:45:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(typeOf cardNumber == null){
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else{
+    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`);
+  }
 }
 addToCart("banana");
 addToCart("lemon");
