3afd9a8da98efc4164fa145aefb1062bfb84b453
Fernando R González <rumbaut2288@gmail.com>
2017-11-11 17:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function viewCart() {
     var i = 1;
    cart.forEach(function(obj, index){
      for(var property in obj){
-       ret = ret + `${property} at $${obj[property]}.` + i < cart.length?',':'';
+       ret = ret + `${property} at $${obj[property]}.`;
 console.log(ret + `${property} at $${obj[property]}.`);
      }
    });
